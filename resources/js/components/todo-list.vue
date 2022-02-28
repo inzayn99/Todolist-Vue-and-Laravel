c96f58b673608f7c0247e80143cd163c20561e2a
inzayn <hungrymindinside99@gmail.com>
2022-02-28 15:12:51 +0545

    config

---
+++ resources/js/components/todo-list.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+<h1>google</h1>
+    </div>
+</template>
+
+
+
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,23 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 
-        <title>TodoList</title>
+        <title>Vue</title>
 
         <!-- Fonts -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 
     </head>
     <body>
     <div id="app">
+
         <todo />
 
     </div>
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     </body>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
 </html>
+<script>
+    import TodoList from "../js/components/todo";
+    export default {
+        components: {Todo}
+    }
+</script>
