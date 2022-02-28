085e1ae87500cf9794bab3e04af55b8bad63bded
inzayn <hungrymindinside99@gmail.com>
2022-02-28 16:23:29 +0545

    npm run watch

--- public/js/app.js
+++ public/js/app.js
@@ -27880,16 +27880,16 @@ var staticRenderFns = [
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "container" }, [
-      _c("div", { staticClass: "row justify-content-center" }, [
+      _c("div", { staticClass: "row justify-content-center mt-5" }, [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card" }, [
             _c("div", { staticClass: "card-header" }, [
-              _vm._v("Welcome to Vue.JS"),
+              _vm._v("Welcome to Vue.jS"),
             ]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
               _vm._v(
-                "\n                    I'm an example component.\n                "
+                "\n                    I'm an example goo.\n                "
               ),
             ]),
           ]),
--- resources/js/components/todo.vue
+++ resources/js/components/todo.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center mt-5">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Welcome to Vue.jS</div>
 
                     <div class="card-body">
                         I'm an example goo.
